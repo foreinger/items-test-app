e8296962968f57d339b1d338a3a2c7818b9d9fd8
alex-moroz <foreinger13@gmail.com>
2023-11-23 11:11:33 +0200

    added ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {importProvidersFrom, NgModule} from '@angular/core';
+import {importProvidersFrom, NgModule, isDevMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -8,6 +8,9 @@ import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {MatDialogModule} from "@angular/material/dialog";
 import {AuthInterceptor} from "./core/interceptors/auth.interceptor";
 import {ErrorHandlerInterceptor} from "./core/interceptors/error.interceptor";
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import {ErrorHandlerInterceptor} from "./core/interceptors/error.interceptor";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [
     importProvidersFrom(MatDialogModule),
